8352716042feb759187aa04373f923683d030c0b
enrico <enricocasadei.enrico@gmail.com>
2020-11-05 12:33:55 +0100

    start ok

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,9 @@
 /* craco.config.js */
-const fs = require("fs");
+const fs = require('fs');
 
 module.exports = {
   devServer: {
-    https: true,
     port: 9000,
     disableHostCheck: true,
-    key: fs.readFileSync("localhost-key.pem"),
-    cert: fs.readFileSync("localhost.pem"),
   },
 };
